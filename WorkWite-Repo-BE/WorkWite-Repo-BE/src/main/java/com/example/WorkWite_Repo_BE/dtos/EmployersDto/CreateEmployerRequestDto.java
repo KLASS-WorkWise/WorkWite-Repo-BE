06a0cca6365b8dfb0fab5db1cd9a4ddd927375b8
VÕ KHẮC ĐOÀI <you@example.com>
2025-07-31 15:14:08 +0700

    checkout

--- WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/dtos/EmployersDto/CreateEmployerRequestDto.java
+++ WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/dtos/EmployersDto/CreateEmployerRequestDto.java
@@ -1,5 +1,24 @@
-package com.example.WorkWite_Repo_BE.dtos.EmployersDto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateEmployerRequestDto {
-
+    @NotBlank
+    private Long userId;
+    private String companyName;
+    private String website;
+    private String logo_url;
+    private String address;
+    private String email;
+    private String industry;
+    private String culture;
+    private String benefits;
+    private String media_url;
 }
--- WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/services/EmployersService.java
+++ WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/services/EmployersService.java
@@ -1,4 +1,103 @@
 package com.example.WorkWite_Repo_BE.services;
 
+import com.example.WorkWite_Repo_BE.dtos.EmployersDto.EmployerResponseDto;
+import com.example.WorkWite_Repo_BE.dtos.EmployersDto.PaginatedEmployerRespondeDto;
+import com.example.WorkWite_Repo_BE.entities.Employers;
+import com.example.WorkWite_Repo_BE.repositories.EmployersJpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class EmployersService {
+    private final EmployersJpaRepository employersJpaRepository;
+
+
+    public EmployersService(EmployersJpaRepository employersJpaRepository) {
+        this.employersJpaRepository = employersJpaRepository;
+    }
+
+    private EmployerResponseDto convertToDto(Employers employers) {
+        return new EmployerResponseDto(
+                employers.getUserId(),
+                employers.getCompanyName(),
+                employers.getWebsite(),
+                employers.getLogoUrl(),
+                employers.getAddress(),
+                employers.getEmail(),
+                employers.getIndustry(),
+                employers.getCulture(),
+                employers.getBenefits(),
+                employers.getMediaUrls()
+        );
+    }
+
+    public PaginatedEmployerRespondeDto getAllEmployers(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Employers> employersPage = this.employersJpaRepository.findAll(pageable);
+
+        List<EmployerResponseDto> employerDto = employersPage.getContent().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+
+        return PaginatedEmployerRespondeDto.builder()
+                .data(employerDto)
+                .pageNumber(employersPage.getNumber())
+                .pageSize(employersPage.getSize())
+                .totalPages(employersPage.getTotalPages())
+                .totalRecords(employersPage.getTotalElements())
+                .hasNext(employersPage.hasNext())
+                .hasPrevious(employersPage.hasPrevious())
+                .build();
+    }
+    public EmployerResponseDto createEmployer(EmployerResponseDto employerResponseDto) {
+        Employers employers = new Employers();
+        employers.setUserId(employerResponseDto.getUserId());
+        employers.setCompanyName(employerResponseDto.getCompanyName());
+        employers.setWebsite(employerResponseDto.getWebsite());
+        employers.setLogoUrl(employerResponseDto.getLogo_url());
+        employers.setAddress(employerResponseDto.getAddress());
+        employers.setEmail(employerResponseDto.getEmail());
+        employers.setIndustry(employerResponseDto.getIndustry());
+        employers.setCulture(employerResponseDto.getCulture());
+        employers.setBenefits(employerResponseDto.getBenefits());
+        employers.setMediaUrls(employerResponseDto.getMedia_url());
+
+        Employers createdEmployer = this.employersJpaRepository.save(employers);
+
+        return convertToDto(createdEmployer);
+    }
+
+    public EmployerResponseDto getEmployerById(Long id) {
+        Employers employer = this.employersJpaRepository.findById(id).orElse(null);
+
+        return convertToDto(employer);
+    }
+
+    public void deleteEmployerById(Long id) {
+        this.employersJpaRepository.deleteById(id);
+    }
+    public EmployerResponseDto updateEmployerById(Long id, EmployerResponseDto employerResponseDto) {
+        Employers employer = this.employersJpaRepository.findById(id).orElse(null);
+        assert  employer != null;
+        employer.setWebsite(employerResponseDto.getWebsite());
+        employer.setLogoUrl(employerResponseDto.getLogo_url());
+        employer.setAddress(employerResponseDto.getAddress());
+        employer.setEmail(employerResponseDto.getEmail());
+        employer.setIndustry(employerResponseDto.getIndustry());
+        employer.setCulture(employerResponseDto.getCulture());
+        employer.setBenefits(employerResponseDto.getBenefits());
+        employer.setMediaUrls(employerResponseDto.getMedia_url());
+        employer.setCompanyName(employerResponseDto.getCompanyName());
+
+        Employers updatedEmployer = this.employersJpaRepository.save(employer);
+
+        return convertToDto(updatedEmployer);
+
+    }
 }
