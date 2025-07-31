b047bb87f96802a162eb067c997f2a2822a3ea5b
VÕ KHẮC ĐOÀI <you@example.com>
2025-07-31 15:37:02 +0700

    update

--- WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/entities/Employers.java
+++
@@ -1,4 +0,0 @@
-package com.example.WorkWite_Repo_BE.entities;
-
-public class Employers {
-}
--- WorkWite-Repo-BE/WorkWite-Repo-BE/src/main/java/com/example/WorkWite_Repo_BE/repositories/EmployersJpaRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.WorkWite_Repo_BE.repositories;
-
-public interface EmployersJpaRepository {
-}
