fa83ab5c709ac31896214b007885011e30a0ba36
Lê Nguyễn Quốc Huy <huylnq2.21it@vku.udn.vn>
2025-08-07 23:43:07 +0700

    update with docker

--- src/main/java/com/example/WorkWite_Repo_BE/dtos/UserDto/UserResponseDto.java
+++ src/main/java/com/example/WorkWite_Repo_BE/dtos/UserDto/UserResponseDto.java
@@ -1,26 +1,13 @@
 package com.example.WorkWite_Repo_BE.dtos.UserDto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
-import java.io.Serializable;
-import java.util.List;
-
 @Data
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
-public class UserResponseDto implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    //Thêm field mới
-    //Xóa field không quan trọng , ko sinh ra ID mới khi tác động
-
-    private String id;
+public class StudentResponseDto {
+    private Long id;
     private String name;
     private String email;
-    private List<RoleResponseDto> roles;
+    private String address;
 }
