e0e18f2f30f2cbb4a130e6ec7f1b4cd63ce4aa5b
VÕ KHẮC ĐOÀI <you@example.com>
2025-09-12 09:55:29 +0700

    update code

--- src/main/java/com/example/WorkWite_Repo_BE/config/WebConfig.java
+++ src/main/java/com/example/WorkWite_Repo_BE/config/WebConfig.java
@@ -16,8 +16,7 @@ public class WebConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000","http://localhost:5173","http://localhost:3001", "http://localhost:5174")
                         .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
